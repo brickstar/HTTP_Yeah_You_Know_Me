64e5c70406acef358573f13ff51e67d438b57443
brickstar <brickstar@gmail.com>
2018-02-12 23:25:20 -0700

    parser

--- lib/response_parser.rb
+++ lib/response_parser.rb
@@ -1,6 +1,35 @@
 class ResponseParser
 
+  def initialize
+    @verb     = vpp[0]
+    @path     = vpp[1]
+    @protocol = vpp[2]
+  end
+
   def parse
-    verb_path_protocol = @request_lines.shift.split(" ")
-    hash = @request_lines.map {|x| x.split(":",2)}.to_h
+    vpp = @request_lines.shift.split(" ") #array of [verb, path, protocol]
+    hash = @request_lines.map {|x| x.split(":", 2)}.to_h # hash of rest of required lines
+  end
+
+  def debug_information
+    @verb     =
+    @path     =
+    @protocol =
+    host     =
+    port     =
+    origin   =
+    accept   =
+
+    <<-END
+      <pre>
+      Verb: #{verb}
+      Path: #{path}
+      Protocol: #{protocol}
+      Host: #{host}
+      Port: #{port}
+      Origin: #{origin}
+      Accept: #{accept}
+      </pre>
+    END
+  end
 end
